185d590e4338aab6bcfd28f4b48a63156cbd08d8
Tomas Malfettani <tomasmalfettani@gmail.com>
2023-08-02 13:57:21 -0300

    Estilo menu, carrito overlay

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const cartIcon = document.querySelector('.bi-bag-fill');
+    const cartOverlay = document.querySelector('.cart-overlay');
+  
+    cartIcon.addEventListener('click', function() {
+      cartOverlay.classList.toggle('open');
+    });
+  
+    cartOverlay.addEventListener('click', function(event) {
+      if (event.target === cartOverlay) {
+        cartOverlay.classList.remove('open');
+      }
+    });
+  });
+  
