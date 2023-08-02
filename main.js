e65952245f91ba60de26525bebc1ab4bd3c6187e
Tomas Malfettani <tomasmalfettani@gmail.com>
2023-08-02 16:23:47 -0300

    Foto personaje @medias

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const cartIcon = document.querySelector('.bi-bag-fill');
     const cartOverlay = document.querySelector('.cart-overlay');
-  
-    cartIcon.addEventListener('click', function() {
-      cartOverlay.classList.toggle('open');
+
+    cartIcon.addEventListener('click', function () {
+        cartOverlay.classList.toggle('open');
     });
-  
-    cartOverlay.addEventListener('click', function(event) {
-      if (event.target === cartOverlay) {
-        cartOverlay.classList.remove('open');
-      }
+
+    cartOverlay.addEventListener('click', function (event) {
+        if (event.target === cartOverlay) {
+            cartOverlay.classList.remove('open');
+        }
     });
-  });
-  
+});
