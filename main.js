7bba229c0470e1f9aea033ffc4b70b77432e4825
Tomas Malfettani <tomasmalfettani@gmail.com>
2023-08-17 23:32:57 -0300

    Animacion cards y foto

--- main.js
+++ main.js
@@ -13,3 +13,13 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    ScrollReveal().reveal('.animated', {
+        delay: 200,
+        duration: 800,
+        origin: 'bottom',
+        distance: '20px',
+        easing: 'ease-out',
+        reset: true
+    });
+});
